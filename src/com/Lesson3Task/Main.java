945e7c211514d2d37e22fcdae0e18c9d18618167
Brandon-Jordan <egorlyannik@gmail.com>
2021-11-10 23:12:44 +0200

    Completed Task #3 Completed Task #4 Task from Lesson 3

---
+++ src/com/Lesson3Task/Main.java
@@ -0,0 +1,47 @@
+package com.Lesson3Task;
+
+public class Main {
+
+    public static void main(String[] args) {
+        int[][] matrix = {
+                {31,32,33},
+                {11,12,13},
+                {1,2,3},
+                {22,23,24}
+        };
+        printArrayElements(matrix);
+        for (int j = 0; j < matrix.length; j++) {
+            for (int i = 0; i < matrix.length - 1; i++) {
+                int sum = sumOfElementsInArr(matrix[i]);
+                int nextSum = sumOfElementsInArr(matrix[i + 1]);
+                if (sum > nextSum) {
+                    int[] temp = matrix[i];
+                    matrix[i] = matrix[i + 1];
+                    matrix[i + 1] = temp;
+                }
+            }
+        }
+        printArrayElements(matrix);
+    }
+
+    static int sumOfElementsInArr(int[] arr) {
+        int sum = 0;
+        for (int i = 0; i < arr.length; i++) {
+            sum += arr[i];
+        }
+        return sum;
+    }
+
+
+
+    static void printArrayElements(int[][] arr) {
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = 0; j < arr[i].length; j++) {
+                System.out.print(arr[i][j] + "\t");
+            }
+            System.out.println();
+        }
+        System.out.println();
+    }
+
+}
---
+++ src/com/Task4/FirstTask.java
@@ -0,0 +1,30 @@
+package com.Task4;
+
+public class FirstTask {
+    public static boolean isSorted(int[] arr, Main.SortOrder sortOrder) {
+        int[] compArr = sortOrder.getSorted(arr);
+        boolean check = false;
+        if (arrayComparer(arr, compArr))
+            check = true;
+        return check;
+    }
+
+    public static void printIsSorted(int[] arr, Main.SortOrder sortOrder) {
+        if (isSorted(arr, sortOrder)) {
+            System.out.println("Your array is " + sortOrder.name());
+        }
+        else {
+            System.out.println("Your array isn`t " + sortOrder.name());
+        }
+    }
+
+    static boolean arrayComparer(int[] arr, int[] compArr) {
+        boolean theSame = true;
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] != compArr[i]) {
+                theSame = false;
+            }
+        }
+        return theSame;
+    }
+}
